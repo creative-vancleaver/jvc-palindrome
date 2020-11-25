7e167024ad1955164a48801a9610be41546df59f
Jacob Van Cleave <vancleave.jacob@gmail.com>
2020-11-25 07:54:22 -0800

    Initialize repository

---
+++ index.js
@@ -0,0 +1,10 @@
+// Reverse a String
+function reverse(string) {
+  return Array.from(string).reverse().join("");
+}
+
+//returns true for a palindrome, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
