982f7e6e229f6eb2ba38eaf4fe19b00c68ad3eee
Jacob Van Cleave <vancleave.jacob@gmail.com>
2020-11-25 08:56:48 -0800

    Finsih working on refactored palindrome method

--- index.js
+++ index.js
@@ -2,21 +2,30 @@
 module.exports = Phrase;
 
 //Adds `reverse` to all strings
-string.prototype.reverse =  function() {
+String.prototype.reverse =  function() {
   return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object
 function Phrase(content) {
   this.content = content;
-}
+
 
 // Returns content processed for palindrome testing
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  //Returns the letters in the content.
+  //For example:
+  // new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
 
 //returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  return this.processedContent() === this.processedContent().reverse();
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+
 }
