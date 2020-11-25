0e7d527be88961e75b607a6c5fc46ac52ccaf817
Jacob Van Cleave <vancleave.jacob@gmail.com>
2020-11-25 08:08:08 -0800

    Create NPM module package

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// Reverse a String
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Export phrase
+module.exports = Phrase;
+
+//Adds `reverse` to all strings
+string.prototype.reverse =  function() {
+  return Array.from(this).reverse().join("");
 }
+
+// Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+}
+
+// Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
 
 //returns true for a palindrome, false otherwise.
 function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+  return this.processedContent() === this.processedContent().reverse();
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+This will be a sample NPM module created in [Learn Enough JavaScript to Be Dangerous](https://learnenough.com).
+
+At present, the module is in 'alpha' phase of development, but will be updated very shortly.
